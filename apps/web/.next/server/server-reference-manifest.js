bff69175ce5a1ef4b266de464a69d71d0d4d8fd4
Andrew Samaha <andrew.samaha@gmail.com>
2025-09-05 07:05:16 -0400

    chore: Adds **/.next/ to gitignore

--- apps/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QHK/UUggcVSIjpKymFsHV7juPniJnpiwcWdmP3S2xzk=\"\n}"
--- apps/web/.next/static/chunks/5703a_pixi_js_lib_rendering_renderers_gl_WebGLRenderer_mjs_438ed727._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/5703a_pixi_js_lib_1b84e577._.js"
-],
-    source: "dynamic"
-});
