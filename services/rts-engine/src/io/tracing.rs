0facc504799cf849d4325c6fd1c99b8180432a0c
Andrew Samaha <andrew.samaha@gmail.com>
2025-09-13 13:43:24 -0400

    feat: Adds RTS_ENGINE_LOG_LEVEL to control tracing

--- services/rts-engine/src/io/tracing.rs
+++ services/rts-engine/src/io/tracing.rs
@@ -2,7 +2,7 @@ use tracing::Level;
 use tracing_subscriber::EnvFilter;
 
 pub fn init_tracing() {
-    let filter = EnvFilter::try_from_default_env()
+    let filter = EnvFilter::try_from_env("RTS_ENGINE_LOG_LEVEL")
         .unwrap_or_else(|_| EnvFilter::default().add_directive(Level::INFO.into()));
     tracing_subscriber::fmt()
         .with_env_filter(filter)
